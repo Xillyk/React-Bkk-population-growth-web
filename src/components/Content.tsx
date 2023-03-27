725050cf0b177adc815690c2c7e0f12dba57ec40
Xillyk <xillyk@hotmail.com>
2023-03-27 18:05:57 +0700

    tmp content

---
+++ src/components/Content.tsx
@@ -0,0 +1,21 @@
+import { Box, Typography } from "@mui/material";
+
+const Content = () => {
+  return (
+    <>
+      <Typography variant="h3" fontWeight="bold">การเติบโต</Typography>
+      
+      {/* CHART FILTER */}
+      <Box>
+        
+      </Box>
+
+      {/* CHART */}
+      <Box>
+
+      </Box>
+    </>
+  );
+};
+
+export default Content;
