4d54a7729e1f49f04cd5492149940d15b0f78ffa
Xillyk <xillyk@hotmail.com>
2023-03-27 15:35:16 +0700

    create mui theme

---
+++ src/theme.ts
@@ -0,0 +1,35 @@
+
+export const themeSettings = {
+	palette: {
+		primary: {
+			main: "#ED2E7C",
+			light: "#FF8BBB"
+		},
+		background: {
+			default: "#333333",
+		},
+	},
+	typography: {
+		allVariants: {
+      color: "#FFFFFF"
+    },
+		fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+		fontSize: 14,
+		h1: {
+			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+			fontSize: 28,
+		},
+		h2: {
+			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+			fontSize: 24,
+		},
+		h3: {
+			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+			fontSize: 18,
+		},
+		h4: {
+			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+			fontSize: 16,
+		},
+	},
+};
