5d7906b643da908ddcf7ec82d6d9e3f56d3290b2
Xillyk <xillyk@hotmail.com>
2023-03-27 17:40:03 +0700

    set Theme provider and custom them, component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
+import { createTheme } from "@mui/material/styles";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import Homepage from "@/scenes/homepage";
 
 function App() {
+  const theme = useMemo(
+    () =>
+      createTheme(themeSettings, {
+        components: {
+          MuiLink: {
+            styleOverrides: {
+              root: {
+                color: "white",
+                textDecorationColor: "white",
+                "&:hover": {
+                  color: themeSettings.palette.primary.main,
+                },
+                "&:active": {
+                  color: themeSettings.palette.primary.light,
+                },
+                "&:focus": {
+                  color: themeSettings.palette.primary.light,
+                },
+              },
+            },
+          },
+        },
+      }),
+    []
+  );
+
   return (
-    <div className="app"></div>
-  )
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%">
+          <Homepage />
+        </Box>
+      </ThemeProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
