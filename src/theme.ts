17603eff4ce8ca60c23b233a257213a5dd04b701
Xillyk <xillyk@hotmail.com>
2023-03-27 17:18:30 +0700

    format theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,35 +1,34 @@
-
 export const themeSettings = {
-	palette: {
-		primary: {
-			main: "#ED2E7C",
-			light: "#FF8BBB"
-		},
-		background: {
-			default: "#333333",
-		},
-	},
-	typography: {
-		allVariants: {
-      color: "#FFFFFF"
+  palette: {
+    primary: {
+      main: "#ED2E7C",
+      light: "#FF8BBB",
     },
-		fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
-		fontSize: 14,
-		h1: {
-			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
-			fontSize: 28,
-		},
-		h2: {
-			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
-			fontSize: 24,
-		},
-		h3: {
-			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
-			fontSize: 18,
-		},
-		h4: {
-			fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
-			fontSize: 16,
-		},
-	},
+    background: {
+      default: "#333333",
+    },
+  },
+  typography: {
+    allVariants: {
+      color: "#FFFFFF",
+    },
+    fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+    fontSize: 14,
+    h1: {
+      fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+      fontSize: 28,
+    },
+    h2: {
+      fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+      fontSize: 24,
+    },
+    h3: {
+      fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+      fontSize: 18,
+    },
+    h4: {
+      fontFamily: ["Bai Jamjuree", "sans-serif"].join(","),
+      fontSize: 16,
+    },
+  },
 };
